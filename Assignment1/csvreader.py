f4262247a3c790a18caeeed55c7deba1b89867e7
pro2812 <35232938+pranav2812@users.noreply.github.com>
2018-08-15 02:37:09 +1200

    Update csvreader.py

--- Assignment1/csvreader.py
+++ Assignment1/csvreader.py
@@ -44,6 +44,7 @@ if __name__=='__main__':
 	#Parsing the arguments
 	ap = argparse.ArgumentParser()
 	ap.add_argument("-n", "--data_file", required=True,help="path to the csvfile")
+	# parser.add_argument('--aboslute', const='absolute', 
 	#for absolute or square error  #ap.add_argument("-t", "--data_file", required=False,help="path to the csvfile") #----------Change it to true afterwards!!!-------
 	args = vars(ap.parse_args())
 
@@ -55,7 +56,7 @@ if __name__=='__main__':
 
 
 	D=dtree()
-	p=args["name"]
+	p=args["data_file"]
 
 	#Filling arrays of data and attributes respectively
 	cols,att = dtree.readcsv(D,p,cols,att)
@@ -64,20 +65,22 @@ if __name__=='__main__':
 #______________________________________________________________________________________________________________
 
 	#generating random number for dataset split into val and train
-	random_row_number = random.randrange(1,cols.shape[0]*2/3,1)
 	
+	random_row_number = random.randrange(1,cols.shape[1]*2/3,1)
+	# print(random_row_number)
 
 	#Making train and val datasets of sizes 2/3 and 1/3 of orignal datasets respectively
-	train = cols[0:random_row_number,:]
-	val = cols[random_row_number:int(random_row_number*2/3),:]
-	train = np.vstack(train,cols[int(random_row_number*2/3)+1:cols.shape[0],:])
+	train = cols[:,0:random_row_number]
+	val = cols[:,random_row_number:int(random_row_number+cols.shape[1]*1/3)]
+	train = np.hstack((train,cols[:,int(random_row_number+cols.shape[1]*1/3)+1:cols.shape[1]]))
 
-
+	print("train",train)
+	print("val",val)
 	#Making tree on the basis of train dataset
-	parent = d_tree(train,0)
+	# parent = d_tree(train,0)
 
 #_____________________________________________________________________________________________________________
 
-	#Saving the model
-	filename = 'model.csv'
-	pickle.dump(model, open(filename, 'wb'))
+	# #Saving the model
+	# filename = 'model.csv'
+	# pickle.dump(model, open(filename, 'wb'))
