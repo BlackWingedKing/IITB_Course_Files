53a331d8e1b47aaee0d08c8f290b08faa4e4f4aa
Ritesh Goru <ritesh.goru@gmail.com>
2018-09-05 16:00:56 +0530

    everything complete

--- Assignment2/assgmt-2/utils.py
+++ Assignment2/assgmt-2/utils.py
@@ -5,17 +5,16 @@ def rescale(targets,outputs):
 	m = targets.shape[0]
 	targets = np.reshape(targets,(m,1))
 	outputs = np.reshape(outputs,(m,1))
-	#return (2*targets-1),(2*outputs-1)
-	return targets,outputs
+	return (2*targets-1),(2*outputs-1)
+	#return targets,outputs
 
 def square_hinge_loss(targets, outputs):
 	# Write thee square hinge loss here
 	targets1,outputs1 = rescale(targets,outputs)
 	m = targets1.shape[0]
 	l = targets1.shape[1]
-	zeros = np.zeros(m).reshape(m,l)
 	hinge = 1-np.multiply(targets1,outputs1)
-	loss = np.maximum(zeros,hinge)
+	loss = np.maximum(0.0,hinge)
 	mean_loss = (1.0/m)*np.dot(loss,loss)
 	return mean_loss
 
@@ -35,20 +34,21 @@ def perceptron_loss(targets, outputs):
 	targets1,outputs1 = rescale(targets,outputs)
 	m = targets1.shape[0]
 	l = targets1.shape[1]
-	zeros = np.zeros(m).reshape(m,l)
 	percep = -1*np.multiply(targets1,outputs1)
-	loss = np.maximum(zeros,percep)
+	loss = np.maximum(0.0,percep)
 	mean_loss = (1.0/m)*np.sum(loss,axis=0)
 	#print('percep_loss........===',mean_loss)
 	return mean_loss
 
 def L2_regulariser(weights):
 	# Write the L2 loss here
-	return la.norm(weights)
+	#return la.norm(weights,2)
+	return np.sum(np.power(weights,2))
 
 def L4_regulariser(weights):
 	# Write the L4 loss here
-	return la.norm(weights,4)
+	#return la.norm(weights,4)
+	return np.sum(np.power(weights,4))
 
 def square_hinge_grad(weights,inputs, targets, outputs):
 	# Write thee square hinge loss gradient here
@@ -57,18 +57,23 @@ def square_hinge_grad(weights,inputs, targets, outputs):
 	targets1,outputs1 = rescale(targets,outputs)
 	m = targets1.shape[0]
 	l = targets1.shape[1]
-	zeros = np.zeros(m).reshape(m,l)
 	hinge = 1-np.multiply(targets1,outputs1)
-	
-	for i in range(0,m):
-		if(yyhat[i,0]!=0):
-			yyhat[i,0]=1
-
-	return np.random.random(11)
+	check = hinge>0.0
+	grad = np.multiply(hinge,targets1)
+	grad = -2*np.multiply(grad,inputs)
+	mean_grad = (1.0/m)*np.sum(grad,axis=0)
+	return mean_grad
 
 def logistic_grad(weights,inputs, targets, outputs):
 	# Write thee logistic loss loss gradient here
-	return 1.00
+	targets1,outputs1 = rescale(targets,outputs)
+	m = targets1.shape[0]
+	div = np.multiply(targets1,outputs1)
+	div = 1 + np.exp(div)
+	num = -1*np.multiply(targets1,inputs)
+	grad = np.divide(num,div)
+	mean_grad = (1.0/m)*np.sum(grad,axis=0)
+	return mean_grad
 
 def perceptron_grad(weights,inputs, targets, outputs):
 	# Write thee perceptron loss gradient here
@@ -77,39 +82,48 @@ def perceptron_grad(weights,inputs, targets, outputs):
 	targets1,outputs1 = rescale(targets,outputs)
 	m = targets1.shape[0]
 	l = targets1.shape[1]
-	zeros = np.zeros(m).reshape(m,l)
-	yyhat = np.multiply(targets1,outputs1)
-	yyhat = -1*yyhat
-	yyhat = np.maximum(zeros,yyhat)
-	for i in range(0,m):
-		if(yyhat[i,0]!=0):
-			yyhat[i,0]=1
-	grad = np.multiply(targets1,inputs)
-	grad = np.multiply(yyhat,grad)
+	yyhat = -1*np.multiply(targets1,outputs1)
+	yyhat1 = yyhat>0.0
+	grad = -1*np.multiply(targets1,inputs)
+	grad = np.multiply(yyhat1,grad)
 	mean_grad = (1.0/m)*np.sum(grad,axis=0)
 	#produce the array y
 	#print('mean_grad..............',mean_grad)
-	return -1*mean_grad
+	return mean_grad
+
+# def L2_grad(weights):
+# 	# Write the L2 loss gradient here
+# 	# dL2/dw
+# 	m = weights.shape[0]
+# 	norm = L2_regulariser(weights)
+# 	grad = np.divide(weights,norm)
+# 	mean_grad = (1.0/m)*np.sum(grad,axis=0)
+# 	return mean_grad
 
 def L2_grad(weights):
 	# Write the L2 loss gradient here
-	# dL2/dw
 	m = weights.shape[0]
-	norm = L2_regulariser(weights)
-	grad = np.divide(weights,norm)
+	grad = 2*weights
 	mean_grad = (1.0/m)*np.sum(grad,axis=0)
 	return mean_grad
+
+# def L4_grad(weights):
+# 	# Write the L4 loss gradient here
+# 	m = weights.shape[0]
+# 	norm = L4_regulariser(weights)
+# 	grad = np.multiply(weights,weights)
+# 	grad = np.multiply(grad,weights)
+# 	norm1 = np.multiply(norm,norm)
+# 	norm1 = np.multiply(norm1,norm)
+# 	gradient = np.divide(grad,norm1)
+# 	mean_grad = (1.0/m)*np.sum(gradient,axis=0)
+# 	return mean_grad
 
 def L4_grad(weights):
 	# Write the L4 loss gradient here
 	m = weights.shape[0]
-	norm = L4_regulariser(weights)
-	grad = np.multiply(weights,weights)
-	grad = np.multiply(grad,weights)
-	norm1 = np.multiply(norm,norm)
-	norm1 = np.multiply(norm1,norm)
-	gradient = np.divide(grad,norm1)
-	mean_grad = (1.0/m)*np.sum(gradient,axis=0)
+	grad = 4*np.power(weights,3)
+	mean_grad = (1.0/m)*np.sum(grad,axis=0)
 	return mean_grad
 
 loss_functions = {"square_hinge_loss" : square_hinge_loss, 
@@ -124,4 +138,4 @@ regularizer_functions = {"L2": L2_regulariser,
                          "L4": L4_regulariser}
 
 regularizer_grad_functions = {"L2" : L2_grad,
-                              "L4" : L4_grad}                           
+                              "L4" : L4_grad}
