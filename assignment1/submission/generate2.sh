7d112ff156221df0c4767e3ddcd54c1c01256091
BlackWingedKing <ritesh.goru@gmail.com>
2019-09-03 12:51:19 +0530

    img taking a lot of time

---
+++ assignment1/submission/generate2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# remove the 1.txt file
+rm -r 2.txt
+
+names='../instances/i-1.txt ../instances/i-2.txt ../instances/i-3.txt'
+als='round-robin epsilon-greedy ucb kl-ucb thompson-sampling'
+eps='0.002 0.02 0.2'
+hz='50 20 800 3200 12800 51200 204800'
+rs='0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49'
+counter=0
+for name in '../instances/i-3.txt'
+do
+    for al in 'thompson-sampling'
+    do
+        for h in $hz
+        do
+            for r in $rs
+            do
+                if [ "$al" == "epsilon-greedy" ]
+                then
+                    for ep in $eps
+                    do
+                        ./bandit.sh --instance $name --algorithm $al --randomSeed $r --epsilon $ep --horizon $h >> 2.txt
+                        # counter=$((counter+1))
+                    done
+                else
+                    ./bandit.sh --instance $name --algorithm $al --randomSeed $r --epsilon 0.1 --horizon $h >> 2.txt
+                    # counter=$((counter+1))
+                fi
+            done
+        done
+    done
+done
+echo $counter
